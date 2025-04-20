c0cf119ba3999a778cc3db02b1e2b2f2d2779a08
eealvarez <familia_alvarez@hotmail.com>
2025-04-20 13:08:15 -0600

    Twig y Herencia Twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; //nos da métodos de accedo directo (atajos)
 use Symfony\Component\HttpFoundation\Response; //Es una biblioteca de Symfony para Peticiones, Respuesta, Sesión
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
 
     #[Route('/', name: 'home')]
     public function homepage(): Response
     {
+        $tracks = [
+            ['tipoServicio' => 'Academia de computación y mecanografía computarizada', 'costo' => 'Q100'],
+            ['tipoServicio' => 'Desarrollo de software', 'costo' => 'Según análisis'],
+            ['tipoServicio' => 'Servicio de Internet Ilimitado residencial', 'costo' => 'Q150'],
+            ['tipoServicio' => 'Reparación de computadoras', 'costo' => 'Según análisis'],
+        ];
+
         // die('INFUNISA');
-        return new Response('<h1>INFUNISA</h1>');
+        // return new Response('<h1>INFUNISA</h1>');
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'Infuisa',
+            'servicios' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}', name: 'browse')]
